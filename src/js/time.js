855be0d5443a8f0957d2446b9e1613f34dfe35f2
Constantine Linardakis <conslina9668@granitesd.org>
2021-12-14 08:59:29 -0700

    FIXED TIMER

--- src/js/time.js
+++ src/js/time.js
@@ -4,32 +4,34 @@ var countDownDate = new Date("Jan 12, 2022 12:34:52").getTime();
 
 // MAKES TIMER FUNCTION UPDATE EVERY SECOND
 
-var myfunc = setInterval(function(){}, 1000)
+var myfunc = setInterval(function(){
 
-// CALCULATING REMAINING TIME IN DAYS, HOURS, MINUTES, SECONDS
-
-var now = new Date().getTime();
-var timeleft = countDownDate - now;
+    // CALCULATING REMAINING TIME IN DAYS, HOURS, MINUTES, SECONDS
     
-var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
-// DISPLAYS OUTPUT TO USERS 
+    // DISPLAYS OUTPUT TO USERS 
 
-document.getElementById("days").innerHTML = days + "d "
-document.getElementById("hours").innerHTML = hours + "h " 
-document.getElementById("mins").innerHTML = minutes + "m " 
-document.getElementById("secs").innerHTML = seconds + "s"
+    document.getElementById("days").innerHTML = days + "d "
+    document.getElementById("hours").innerHTML = hours + "h " 
+    document.getElementById("mins").innerHTML = minutes + "m " 
+    document.getElementById("secs").innerHTML = seconds + "s"
 
-// DISPLAYS MESSAGE WHEN THE TIMER IS OVER (OPTIONAL)
+    // DISPLAYS MESSAGE WHEN THE TIMER IS OVER (OPTIONAL)
 
-if (timeleft < 0) {
-    clearInterval(myfunc);
-    document.getElementById("days").innerHTML = ""
-    document.getElementById("hours").innerHTML = "" 
-    document.getElementById("mins").innerHTML = ""
-    document.getElementById("secs").innerHTML = ""
-    document.getElementById("end").innerHTML = "CELEB LIVE NEW EVENT HERE!";
-}
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "CELEB LIVE NEW EVENT HERE!";
+    }
+}, 1000)
+
